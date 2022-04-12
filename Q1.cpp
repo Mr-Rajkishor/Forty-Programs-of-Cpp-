b751de22f4f9873e622f3e0c93de5c7bb773afe8
Raj kishor <rajkishor6091@gmail.com>
2022-04-12 22:43:16 +0530

    Q1.C++

---
+++ Q1.cpp
@@ -0,0 +1,10 @@
+/*                                    Program:-1
+                    Write a c++ program to print “Hello World”.
+                                                                                */
+
+#include<iostream>
+int main()
+{
+	std::cout<<"Hello World";
+return 0;
+}

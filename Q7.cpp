ec364ab9ee54400f83961355ffbc3cafba56edc5
Raj Kishor <rajkishor6091@gmail.com>
2022-04-14 23:44:18 +0530

    Q7 solve

---
+++ Q7.cpp
@@ -0,0 +1,17 @@
+/*                         program:- 7;
+Write a c++ program to interchange the value of two variables using third variable
+                                                                                                    */
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,t;
+    cout<<"Enter two number a and b:- ";
+	cin>>a>>b;
+	cout<<"_______after interchange number________"<<endl<<"\t\ta="<<a<<"\tb="<<b;
+	t=a;
+	a=b;
+	b=t;
+	cout<<"\n_______before interchange number_______"<<endl<<"\t\ta="<<a<<"\tb="<<b;
+	return 0;
+}

741c6eb886154a0f7ff26b458c31db9cd5929338
Raj kishor <rajkishor6091@gmail.com>
2022-04-16 22:31:50 +0530

    add two numbers

---
+++ Q2.cpp
@@ -0,0 +1,15 @@
+/*                            Program:-2
+                Write a C++ program to add two numbers.
+                                                                    */
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,sum;
+	cout<<"Enter the two number:- ";
+	cin>>a;
+	cin>>b;
+	sum=a+b;
+	cout<<"sum= "<<sum;
+return 0;
+}

7cd9a707707ea42750882a8b3d56f800c36c3739
Raj kishor <rajkishor6091@gmail.com>
2022-04-16 22:33:42 +0530

    ultiply two float numbers

---
+++ Q4.cpp
@@ -0,0 +1,17 @@
+/*                                Program:- 4
+        Write a C++ program to perform arithmetic operation ( +, -, *, / ) on two integer 
+        number.
+                                                       */
+#include<iostream>
+using namespace std;
+int main()
+{
+        int a,b;
+		cout<<"Enter the two number:- ";
+		cin>>a>>b;
+		cout<<"sum= "<<a+b<<"\n";
+		cout<<"Substraction= "<<a-b<<"\n";
+		cout<<"Multiply= "<<a*b<<"\n";
+		cout<<"Division= "<<a/b<<"\n";
+return 0;
+}

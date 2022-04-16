c4fe3afdf59bee3d322c1abb864a84171cd23fe8
Raj kishor <rajkishor6091@gmail.com>
2022-04-16 22:32:37 +0530

    multiply two float numbers

---
+++ Q3.cpp
@@ -0,0 +1,12 @@
+/*                          Program:- 3
+        Write a C++ program to multiply two float numbers.
+                                                                       */
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a,b;
+	cout<<"Enter two number:- ";
+	cin>>a>>b;
+	cout<<"multiply= "<<a*b;
+}
